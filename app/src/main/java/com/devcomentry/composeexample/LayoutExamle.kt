9afbbd8c2da7d45277d3884fb92daccef3628203
Nguyen Van Tan <doctorblue.king@gmail.com>
2022-05-09 22:10:17 +0700

    Layout example

--- app/src/main/java/com/devcomentry/composeexample/LayoutExamle.kt
+++ app/src/main/java/com/devcomentry/composeexample/LayoutExamle.kt
@@ -1,3 +1,37 @@
 package com.devcomentry.composeexample
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
 
+@Composable
+@Preview(showSystemUi = true)
+fun Preview(){
+//    ColumnTest()
+    RowTest()
+}
+
+
+@Composable
+fun ColumnTest(){
+    Column(verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Hello")
+        Text("Who are u?")
+    }
+}
+@Composable
+fun RowTest(){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text("Hello")
+        Text("Who are u?")
+    }
+}
