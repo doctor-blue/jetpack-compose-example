768f51f0423945f8758e6dfd8e85604deedadd63
Nguyen Van Tan <doctorblue.king@gmail.com>
2022-06-03 18:21:03 +0700

    Update UI

--- app/src/main/java/com/devcomentry/plants/presentation/home/HomeList.kt
+++ app/src/main/java/com/devcomentry/plants/presentation/home/HomeList.kt
@@ -2,18 +2,22 @@ package com.devcomentry.plants.presentation.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.devcomentry.plants.data.DataResources
 import com.devcomentry.plants.data.Plant
-import com.devcomentry.plants.ui.CommonItemRadius
+import com.devcomentry.plants.ui.*
 
 @Composable
 @Preview
@@ -25,12 +29,41 @@ fun HomeListPreview() {
 fun HomeItem(plant: Plant) {
     Card(
         shape = RoundedCornerShape(CommonItemRadius),
-        backgroundColor = Color.White
+        backgroundColor = Color.White,
+        modifier = Modifier
+            .padding(0.dp)
+            .padding(Space10)
     ) {
-        Image(
-            painter = painterResource(plant.image),
-            contentDescription = "Home Item"
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Column {
+                Image(
+                    painter = painterResource(plant.image),
+                    contentDescription = "Home Item",
+                    modifier = Modifier.fillMaxWidth()
+                )
+                PlantsMediumText(
+                    text = plant.name,
+                    color = Color.Black,
+                    fontSize = TextSize14,
+                    modifier = Modifier
+                        .padding(
+                            horizontal = SpaceScreenSuperSmall
+                        )
+                        .padding(top = SpaceScreenSuperSmall)
+                )
+                PlantsBoldText(
+                    text = "${plant.price} ${plant.unit}",
+                    color = Color.Black,
+                    fontSize = TextSize14,
+                    modifier = Modifier.padding(
+                        horizontal = SpaceScreenSuperSmall
+                    )
+                )
+            }
+        }
     }
 }
 
@@ -40,12 +73,12 @@ fun HomeList(
     list: List<Plant> = DataResources.plants
 ) {
     LazyVerticalGrid(
-        cells = GridCells.Adaptive(minSize = 60.dp),
+        cells = GridCells.Fixed(2),
         state = rememberLazyListState(),
         contentPadding = PaddingValues(10.dp)
     ) {
         items(list) {
-            HomeItem(it)
+            HomeItem(plant = it)
         }
     }
 }
--- app/src/main/java/com/devcomentry/plants/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/devcomentry/plants/presentation/home/HomeScreen.kt
@@ -1,14 +1,8 @@
 package com.devcomentry.plants.presentation.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import com.devcomentry.plants.ui.PlantsBoldText
 
 @Composable
 fun HomeScreen() {
-    Column {
-        PlantsBoldText("Hello", Color.Black)
-    }
+    HomeList()
 }
